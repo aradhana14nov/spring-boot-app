f2d12070420c568c5406dbde3e015929f495c274
aradhana14nov <73100123+aradhana14nov@users.noreply.github.com>
2021-03-14 00:47:05 +0530

    added application files

---
+++ src/main/java/com/mywebsite/sample/keycloak/MywebsiteKeycloakSampleApplication.java
@@ -0,0 +1,13 @@
+package com.mywebsite.sample.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebsiteKeycloakSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywebsiteKeycloakSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mywebsite/sample/keycloak/MywebsiteKeycloakSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywebsite.sample.keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywebsiteKeycloakSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
